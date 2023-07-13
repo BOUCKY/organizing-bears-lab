8893d846083023c2f7b27eb7122c52c34a47ece7
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-07-13 16:20:39 -0400

    Complete Lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 10, 'M', 'Brown', 'Sweet', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 3, 'M', 'Red', 'Rowdy', 0);
+INSERT INTO bears VALUES (3, 'Tabitha', 20, 'F', 'Grey', 'Motherly', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 50, 'M', 'Brown', 'Strict', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 15, 'F', 'White', 'Bitchy', 0);
+INSERT INTO bears VALUES (6, 'Grinch', 100, 'M', 'Green', 'Mean', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 54, 'F', 'Red', 'Makes Food', 1);
+INSERT INTO bears VALUES (8, NULL, 10, 'M', 'Blue', 'Mysterious', 0);
